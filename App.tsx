7d88eeed263882e43c501abcb23ef15003ec587d
Harbovskyi Serhii <garbovsky.s@gmail.com>
2024-08-05 22:55:41 +0100

    Fix Android: turn of debug for Android as it does not work with Hermes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {NativeModules} from 'react-native';
+import {NativeModules, Platform} from 'react-native';
 import {Provider} from 'react-redux';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 import {store} from './src/store/store';
 import MainScreen from './src/screens/MainScreen';
 
-if (__DEV__) {
+if (__DEV__ && Platform.OS === 'ios') {
   NativeModules.DevSettings.setIsDebuggingRemotely(true);
 }
 
